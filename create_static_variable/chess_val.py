68a612a51fb0ce9c00adaf3336bc32d276a91cf5
thq <18868195165@163.com>
2024-12-02 22:22:17 +0800

    ss

---
+++ create_static_variable/chess_val.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   mes_load.py    
+@Contact :   hqtong@epsoft.com.cn
+@Desciption：
+------------ 
+chess_val
+------------ 
+@Modify Time      @Author    @Version    
+------------      -------    --------  
+2024/11/21 15:22   hqtong      1.0         
+'''
+# import lib
+import json
+import os
+from config.set_config import *
+fn = 'chess_message_2.txt'
+fn_1 = 'chess_val.json'
+infn = os.path.join(Config_base.ref,fn)
+outfn = os.path.join(Config_base.data_warehouse,fn_1)
+aim_dict = {}
+
+with open(infn,'r',encoding='utf_8') as f:
+    for line in f:
+        mes = line.strip().split("：")
+        chess_val = mes[0]
+        aim_dict[chess_val] = []
+        aim_dict[chess_val] = mes[1].split('、')
+print(aim_dict)
+
+Config_base.dumps_data(aim_dict,outfn)
+# with open(outfn, "w+", encoding='utf-8') as f:
+#     f.write(json.dumps(aim_dict, ensure_ascii=False))
